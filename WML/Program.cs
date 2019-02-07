19452bbc263318157e254b157ad7afb3fb5b5d60
1Gregory <6squaress@gmail.com>
2019-02-07 20:13:58 +0300

    Don't close them

--- WML/Program.cs
+++ WML/Program.cs
@@ -1,27 +1,47 @@
 ﻿using System;
 using System.IO;
-/*
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-*/
 
 namespace WML
 {
+    class Token
+    {
+
+    }
+
     class Program
     {
         static void Main(string[] argvs)
         {
             if (argvs.Length == 2)
             {
-
+                if (File.Exists(argvs[0]))
+                {
+                    FileStream WML_code_stream = new FileStream(argvs[0], FileMode.Open);
+                    StreamReader WML_code_reader = new StreamReader(WML_code_stream);
+                    string WML_code = WML_code_reader.ReadToEnd();
+                    Token[] tokens = Lexer(WML_code);
+                }
+                else
+                {
+                    Console.WriteLine("    WML can't find this file!\n    To continue press any button...");
+                    Console.ReadKey();
+                }
             }
             else
             {
                 Console.WriteLine("    Usage:\n\n    WML <.wml> <.html>\n    To continue press any button...");
                 Console.ReadKey();
             }
+        }
+
+        string[] dont_close_them = new string[19] {"area", "base", "basefont", "bgsound", "br",
+                                                    "col", "command", "embed", "hr", "img",
+                                                    "input", "isindex", "keygen", "link", "meta",
+                                                    "param", "source", "track", "wbr"};
+
+        static Token[] Lexer(string wml_code)
+        {
+
         }
     }
 }
