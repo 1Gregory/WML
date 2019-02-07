08b3b251295806fe367fac2bf3a9372ecfc352e2
1Gregory <6squaress@gmail.com>
2019-02-07 16:23:09 +0300

    Start

---
+++ WML/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+/*
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+*/
+
+namespace WML
+{
+    class Program
+    {
+        static void Main(string[] argvs)
+        {
+            if (argvs.Length == 2)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("    Usage:\n\n    WML <.wml> <.html>\n    To continue press any button...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
